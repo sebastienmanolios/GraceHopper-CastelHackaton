e0a98549f4132901cb4853ef273e2538a6a46a7f
AnkiaDev <ankiadev@gmail.com>
2021-05-06 12:13:19 +0200

    created config file for mongo db connection, update server.js

--- back/server.js
+++ back/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
