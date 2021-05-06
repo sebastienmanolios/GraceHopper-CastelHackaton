6f644be5e1d04bc9a5c833cafcb326e5047bdc39
sebastienmanolios <sebastien.manolios@gmail.com>
2021-05-06 14:54:16 +0200

    adding some components

--- front/src/components/AppNavBar.vue
+++ front/src/components/AppNavBar.vue
@@ -1,30 +1,34 @@
 <template>
-  <v-app>
-    <nav>
-      <v-toolbar id="toolbar">
-        <v-toolbar-title>
-          <span class="px-3">Title</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" class="hidden-md-and-up" @click="toggleMenu = !toggleMenu">
-          <v-icon>mdi-menu</v-icon>
-        </v-btn>
-        <v-menu
-          v-model="showMenu"
-          :absolute="toggleMenu"
-          :attach="'#toolbar'"
-          :content-class="'px-2 elevation-0'"
-          :nudge-top="$vuetify.breakpoint.smAndDown?-52:-12"
-          min-width="100%">
-            <div class="d-flex flex-column flex-md-row justify-end">
-                <v-btn class="ma-1">theme1</v-btn>
-                <v-btn class="ma-1">theme2</v-btn>
-                <v-btn class="ma-1">theme3</v-btn>
-          </div>
-        </v-menu>
-      </v-toolbar>
-    </nav>
-    </v-app>
+  <nav>
+    <v-toolbar id="toolbar">
+      <v-toolbar-title>
+        <span class="px-3">Title</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" class="hidden-md-and-up" @click="toggleMenu = !toggleMenu">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+      <v-menu
+        v-model="showMenu"
+        :absolute="toggleMenu"
+        :attach="'#toolbar'"
+        :content-class="'px-2 elevation-0'"
+        :nudge-top="$vuetify.breakpoint.smAndDown?-52:-12"
+        min-width="100%">
+        <div class="d-flex flex-column flex-md-row justify-end">
+            <v-btn
+              v-for="(link, index) in links" :key="index"
+              class="ma-1"
+              text
+              rounded
+              :to="link.url"
+            >
+            {{ link.label }}
+            </v-btn>
+        </div>      
+      </v-menu>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
@@ -32,7 +36,21 @@
     data() {
         return {
             toggleMenu: false,
-        }
+            links: [
+              {
+                label:'Home',
+                url:'/'
+              },
+              {
+                label:'Login',
+                url:'/login'
+              },
+              {
+                label:'Signup',
+                url:'/signup'
+              },
+            ]     
+          }
     },
     computed: {
         showMenu() {
