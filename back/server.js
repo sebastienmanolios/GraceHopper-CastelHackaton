3ac28933ac4d9d5663bcab8d65910d72eabd5ead
geo <geoboiffils34@gmail.com>
2021-05-07 10:53:18 +0200

    swagger update

--- back/server.js
+++ back/server.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
-const cityRoutes = require("./routes/city"); // import the routes
-const userRoutes = require("./routes/user"); // import the routes
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const cityRoutes = require("./routes/city"); 
+const userRoutes = require("./routes/user"); 
 const connectDB = require("./config/db");
 const cors = require("cors");
 
 dotenv.config();
 connectDB();
-
 const app = express();
+
+//Options Backoffcie Swagger
+const swaggerOptions = {
+  swaggerDefinition:{
+    components: {},
+      info: {
+          title: 'OCC API',
+          version: '1.0.0',
+          description: 'API pour connaître la qualité de l\'air de votre Communauté de commune!'
+      },
+      servers: [
+          {
+              url: "http://localhost:5500",
+          },
+      ],
+  },
+  apis: ['routes/*.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/cities", cityRoutes);
 app.use("/users", userRoutes);
+app.use('/city-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const listener = app.listen(process.env.PORT || 5500, () => {
   console.log(
